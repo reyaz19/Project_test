6321c0430295cb9c58b7f8f9def6b1908d073884
Reyaz Ahmad <reyazahmad0011@gmail.com>
2019-08-05 11:38:00 +0530

    Basic controller function

---
+++ testing_project/controller_execution_basic.py
@@ -0,0 +1,44 @@
+__author__ = 'Reyaz Ahmad'
+
+import subprocess
+
+class ControllerExec:
+
+    def __init__(self):
+        return
+
+    def check_git_version(self):
+        """
+        git version check
+        :return:
+        """
+        cmd = 'pwd'
+        returned_output = subprocess.check_output(cmd)
+        print('Output:', returned_output)
+
+    def login(self):
+        """
+                login without user
+                :return:
+        """
+        cmd = "xprctl login"
+        returned_output = subprocess.check_output(cmd)
+        print('Output:', returned_output)
+
+    def login_with_user_name(self):
+        """
+        login with user
+        :return:
+        """
+        cmd = "xprctl login -u user1"
+        returned_output = subprocess.check_output(cmd)
+        print('Output:', returned_output)
+
+    def logout(self):
+        """
+        logout
+        :return:
+        """
+        cmd= "xprctl logout"
+        returned_output=subprocess.check_output(cmd)
+        print('Output:', returned_output)
